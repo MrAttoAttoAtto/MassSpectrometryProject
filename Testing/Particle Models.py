e2a7bb503e3a3a6dfa2b9d878b915ac5ec3945b3
MrAttoAttoAtto <atto.allas@gmail.com>
2020-06-18 18:31:29 +0100

    Added angles and made everything better

--- Testing/Particle Models.py
+++ Testing/Particle Models.py
@@ -1,6 +1,8 @@
 from matplotlib import pyplot as plt
+import matplotlib.patches, matplotlib.lines
 import numpy as np
 import math
+import operator
 import time
 
 
@@ -151,26 +153,27 @@ def is_detected(B: float, r: float, V: float, q: float, m: float, back_length: f
 
     return abs(y) <= slit_width/2
 
-'''
-def show_path_np(B: float, r: float, V: float, e: float, m: float, timestep: float = 10 ** -9) -> None:
-    """
-    Shows a matplotlib plot of the path of a particle (mass m, charge e) through a magnetic field of strength B,
-    radius (of action) r which is perpendicular to the plane of the page, having been accelerated by a voltage V.
 
-    :param B: The strength of the magnetic field in tesla
-    :param r: The radius of the effective area of the magnetic field in metres
-    :param V: The accelerating voltage on the particle (before it enters the field) in Volts (assuming start at rest)
-    :param e: The charge on the particle in electron charges
-    :param m: The mass of the particle in AMUs
-    :return: None
+def is_detected_angle(B: float, r: float, V: float, q: float, m: float, entry_length: float, exit_length: float, angle: float, slit_width: float):
     """
+    Checks is a given fired particle would be detected by the machine
 
-    r_squared = r ** 2
+    :param B:
+    :param r:
+    :param V:
+    :param q:
+    :param m:
+    :param front_length:
+    :param angle:
+    :param back_length:
+    :param slit_width:
+    :return:
+    """
 
     mass = m * 1.66054e-27
-    charge = e * 1.60217662e-19
+    charge = q * 1.60217662e-19
 
-    field = np.array([0, 0, B])
+    angle_radians = 2 * math.pi * (90 - angle) / 360
 
     # Note that this is constant, as the magnetic field can do no work on the particle as it always contributes
     # a force perpendicular to the direction of motion
@@ -179,40 +182,165 @@ def show_path_np(B: float, r: float, V: float, e: float, m: float, timestep: flo
     # energy = 1/2*mass*v^2 ==> v = root(2energy/mass)
     speed = math.sqrt(2 * energy / mass)
 
-    # [x, y, z]
-    velocity = np.array([0, speed, 0])
-    position = np.array([0, -r, 0])
+    path_gradient = np.inf if angle == 0 else math.tan(angle_radians)
 
-    points = []
+    m = path_gradient
+    c = -(entry_length + r)
+    op = (lambda x, y: x - y) if angle >= 0 else (lambda x, y: x + y)
+    entry_x = (op(-2 * c * m, math.sqrt(4 * c ** 2 * m ** 2 - 4 * (m ** 2 + 1) * (c ** 2 - r ** 2)))) / (
+            2 * (m ** 2 + 1)) if path_gradient != np.inf else 0
+    entry_y = -math.sqrt(r ** 2 - entry_x ** 2)
 
-    while max(abs(position)) <= 2 * r:
-        points.append([position[0], position[1]])
+    tangent_gradient = path_gradient
 
-        # If it is inside the field...
-        if position[0] ** 2 + position[1] ** 2 <= r_squared:
-            # Lorentz force w/o electric field
-            force = charge * np.cross(velocity, field)
-        else:
-            force = np.array([0, 0, 0])
+    radius_length = mass * speed / (charge * B)
+    radius_gradient = -1 / tangent_gradient
 
-        # F = ma ==> a = F/m
-        acceleration = force / mass
+    centre_x = radius_length * math.sqrt(1 / (1 + radius_gradient ** 2)) + entry_x
+    centre_y = radius_gradient * centre_x + (entry_y - radius_gradient * entry_x)
 
-        # s = ut + 1/2 * at^2
-        displacement = velocity * timestep + 1 / 2 * acceleration * timestep ** 2
-        position += displacement
+    a = centre_x
+    b = centre_y
+    p = radius_length
+    k = r ** 2 + a ** 2 + b ** 2 - p ** 2
+    determinant = math.sqrt(4 * a ** 4 * r ** 2 + 4 * a ** 2 * b ** 2 * r ** 2 - a ** 2 * k ** 2)
 
-        # a = (v - u)/t ==> v = at + u
-        velocity += acceleration * timestep
+    exit_y = (b * k + determinant) / (2 * (a ** 2 + b ** 2))
+    if abs(exit_y - entry_y) < 1e-12:
+        # It is the other solution!
+        exit_y = (b * k - determinant) / (2 * (a ** 2 + b ** 2))
 
-    #fig, ax = plt.subplots()
+    exit_x = math.sqrt(r ** 2 - exit_y ** 2)
+    if abs(((exit_x - centre_x) ** 2 + (exit_y - centre_y) ** 2) - radius_length ** 2) >= 1e-12 or abs(
+            exit_x - entry_x) < 1e-12:
+        # It is the other x solution!
+        exit_x = -exit_x
 
-    #ax.plot(*zip(*points))
-    #circle = plt.Circle((0, 0), r, color="black", fill=False)
-    #ax.add_artist(circle)
-    #fig.show()
-'''
+    radius_gradient_2 = np.inf if exit_x == centre_x else (exit_y - centre_y) / (exit_x - centre_x)
+    tangent_gradient_2 = -1 / radius_gradient_2
+
+    # exit_v_x = speed * math.sqrt(1/(1+tangent_gradient_2**2))
+    # exit_v_y = tangent_gradient_2 * exit_v_x
+
+    # x^2 + y^2 = distance^2 (y = mx + c simplified)
+    distance_right = (exit_x + 1) ** 2 + (tangent_gradient_2 + exit_y) ** 2
+    distance_left = (exit_x - 1) ** 2 + (exit_y - tangent_gradient_2) ** 2
+
+    going_right = distance_right >= distance_left
+
+    if not going_right:
+        return False
+
+    c = exit_y - tangent_gradient_2 * exit_x
+    final_y = tangent_gradient_2 * (exit_length + r) + c
+
+    return abs(final_y) <= slit_width / 2
 
 
-show_path_diff(0.26, 0.05, 550, 1, 15, 1e-10)
-print(is_detected(0.26, 0.05, 550, 1, 15, 0.04, 6.35e-5))
+def show_path_diff_angle(B: float, r: float, V: float, q: float, m: float, entry_length: float, exit_length: float, angle: float, slit_width: float):
+    """
+
+    :param B:
+    :param r:
+    :param V:
+    :param q:
+    :param m:
+    :param front_length:
+    :param back_length:
+    :param angle:
+    :param slit_width:
+    :return:
+    """
+
+    mass = m * 1.66054e-27
+    charge = q * 1.60217662e-19
+
+    angle_radians = 2 * math.pi * (90 - angle) / 360
+
+    # Note that this is constant, as the magnetic field can do no work on the particle as it always contributes
+    # a force perpendicular to the direction of motion
+    energy = V * charge
+
+    # energy = 1/2*mass*v^2 ==> v = root(2energy/mass)
+    speed = math.sqrt(2 * energy / mass)
+
+    path_gradient = np.inf if angle == 0 else math.tan(angle_radians)
+
+    m = path_gradient
+    c = -(entry_length + r)
+    op = (lambda x, y: x - y) if angle >= 0 else (lambda x, y: x + y)
+    entry_x = (op(-2 * c * m, math.sqrt(4 * c ** 2 * m ** 2 - 4 * (m ** 2 + 1) * (c ** 2 - r ** 2)))) / (
+            2 * (m ** 2 + 1)) if path_gradient != np.inf else 0
+    entry_y = -math.sqrt(r ** 2 - entry_x ** 2)
+
+    tangent_gradient = path_gradient
+
+    radius_length = mass * speed / (charge * B)
+    radius_gradient = -1 / tangent_gradient
+
+    centre_x = radius_length * math.sqrt(1 / (1 + radius_gradient ** 2)) + entry_x
+    centre_y = radius_gradient * centre_x + (entry_y - radius_gradient * entry_x)
+
+    a = centre_x
+    b = centre_y
+    p = radius_length
+    k = r ** 2 + a ** 2 + b ** 2 - p ** 2
+    determinant = math.sqrt(4 * a ** 4 * r ** 2 + 4 * a ** 2 * b ** 2 * r ** 2 - a ** 2 * k ** 2)
+
+    exit_y = (b * k + determinant) / (2 * (a ** 2 + b ** 2))
+    if abs(exit_y - entry_y) < 1e-12:
+        # It is the other solution!
+        exit_y = (b * k - determinant) / (2 * (a ** 2 + b ** 2))
+
+    exit_x = math.sqrt(r ** 2 - exit_y ** 2)
+    if abs(((exit_x - centre_x) ** 2 + (exit_y - centre_y) ** 2) - radius_length ** 2) >= 1e-12 or abs(
+            exit_x - entry_x) < 1e-12:
+        # It is the other x solution!
+        exit_x = -exit_x
+
+    radius_gradient_2 = np.inf if exit_x == centre_x else (exit_y - centre_y) / (exit_x - centre_x)
+    tangent_gradient_2 = -1 / radius_gradient_2
+
+    fig, ax = plt.subplots()
+
+    circle = plt.Circle((0, 0), r, color="black", fill=False)
+    ax.add_artist(circle)
+
+    exit_above_line = exit_y > (radius_gradient * exit_x + (entry_y - radius_gradient * entry_x))
+    op = (lambda x, y: x - y) if exit_above_line else (lambda x, y: x + y)
+    end_angle = op((180 - angle), 180 * math.acos(1 - ((entry_x - exit_x) ** 2 + (entry_y - exit_y) ** 2) / (2 * radius_length**2)) / math.pi)
+    arc_path = matplotlib.patches.Arc((centre_x, centre_y), radius_length * 2, radius_length * 2, 0,
+                                      end_angle, 180 - angle, color="green")
+
+    if angle != 0:
+        x1 = np.linspace(0, entry_x, 1000)
+        y1 = tangent_gradient * x1 - r - entry_length
+    else:
+        x1 = np.zeros(1000)
+        y1 = np.linspace(-r-entry_length, -r, 1000)
+
+    # x^2 + y^2 = distance^2 (y = mx + c simplified)
+    distance_right = (exit_x + 1) ** 2 + (tangent_gradient_2 + exit_y) ** 2
+    distance_left = (exit_x - 1) ** 2 + (exit_y - tangent_gradient_2) ** 2
+
+    going_right = distance_right >= distance_left
+
+    end_x = r+exit_length if going_right else -r-exit_length
+    x2 = np.linspace(exit_x, end_x, 1000)
+    y2 = tangent_gradient_2 * x2 + (exit_y - tangent_gradient_2 * exit_x)
+
+    ax.plot(x1, y1)
+    ax.plot(x2, y2)
+
+    ax.vlines(r + exit_length, -abs(y2[-1])*2, -slit_width / 2, color="purple")
+    ax.vlines(r + exit_length, slit_width / 2, abs(y2[-1])*2, color="purple")
+
+    ax.add_patch(arc_path)
+    lim = max(1.2 * r + entry_length, 1.2 * r + exit_length)
+    ax.set(xlim=(-lim, lim), ylim=(-lim, lim))
+    fig.show()
+
+
+
+show_path_diff_angle(0.6752985451, 0.05, 550, 1, 100, 0.05, 0.05, 3, 6.7e-5)
+print(is_detected_angle(0.6752985451, 0.05, 550, 1, 100, 0.05, 0.05, 3, 6.7e-5))
